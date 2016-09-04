335916592e45ceff5de06b75648efe57f7df7df1
Cheng-Yu Pai <mail4charliepai@gmail.com>
2016-09-03 18:47:42 -0700

    Add simple UT for reading crops.csv file

--- core/src/test/com/pipai/sfa/battle/schemaindex/CropSchemaIndexTest.java
+++ core/src/test/com/pipai/sfa/battle/schemaindex/CropSchemaIndexTest.java
@@ -1,11 +1,20 @@
 package com.pipai.sfa.battle.schemaindex;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class CropSchemaIndexTest {
+import com.badlogic.gdx.Gdx;
+import com.google.common.collect.ImmutableList;
+import com.pipai.libgdx.test.GdxMockedTest;
+import com.pipai.sfa.battle.components.CropSchema;
+
+public class CropSchemaIndexTest extends GdxMockedTest {
 
 	@Test
 	public void testReadActualFile() {
+		CropSchemaIndex cropSchemaIndex = new CropSchemaIndex(Gdx.files.internal("data/crops.csv"));
+		ImmutableList<CropSchema> cropSchemas = cropSchemaIndex.getAllCropSchemas();
+		Assert.assertTrue(cropSchemas.size() > 0);
 	}
 
 }
