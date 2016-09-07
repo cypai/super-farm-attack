2aa125031afc9766d5c463db65d0b87413cea487
Cheng-Yu Pai <mail4charliepai@gmail.com>
2016-09-06 23:33:45 -0700

    Make resolution larger

--- desktop/src/main/java/com/pipai/sfa/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/pipai/sfa/desktop/DesktopLauncher.java
@@ -12,6 +12,8 @@ public final class DesktopLauncher {
 	// SUPPRESS CHECKSTYLE UncommentedMain This is the main entry point
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 768;
 		new LwjglApplication(new SfaGame(), config);
 	}
 }
